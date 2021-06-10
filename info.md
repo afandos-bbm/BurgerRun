f97adacd110e49ec6d45fffedbfef089877ef4a0
Alejandro <alejandrofan2@gmail.com>
2021-06-10 10:04:10 +0200

    Added animations class

---
+++ info.md
@@ -0,0 +1,13 @@
+Texturas que necesito:
+Jugador =
+	Jugador quieto
+	Jugador corriendo
+	jugador corriendo 2
+	jugador saltando
+	jugador agachado
+Visual del juego =
+	fondo
+	suelo
+Obstaculos =
+	bloque
+	pinchos
---
+++ src/alejandrofan2/burgerRun/objects/Animation.java
@@ -0,0 +1,55 @@
+package alejandrofan2.burgerRun.objects;
+
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+
+public class Animation {
+	
+	private int speed;
+	private int frames;
+	
+	private int index = 0;
+	private int count = 0;
+	
+	private BufferedImage[] images;
+	private BufferedImage currentImage;
+	
+	public Animation(int speed, BufferedImage... args) {
+		this.speed = speed;
+		images = new BufferedImage[args.length];
+		for (int i = 0; i < args.length; i++) {
+			images[i] = args[i];
+		}
+		frames = args.length;
+	}
+	
+	public void runAnimation() {
+		index++;
+		if (index > speed) {
+			index = 0;
+			nextFrame();
+		}
+	}
+	
+	private void nextFrame() {
+		for (int i = 0; i < frames; i++) {
+			if (count == i) {
+				currentImage = images[i];
+			}
+		}
+		count++;
+		
+		if (count > frames) {
+			count = 0;
+		}
+	}
+	
+	public void drawAnimation(Graphics g, int x, int y) {
+		g.drawImage(currentImage, x, y, null);
+	}
+	
+	public void drawAnimation(Graphics g, int x, int y, int resizeX, int resizeY) {
+		g.drawImage(currentImage, x, y, resizeX, resizeY, null);
+	}
+}
+
